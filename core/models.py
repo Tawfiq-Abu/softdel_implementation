c78cdf02904c1af1ad61546b7d573b0ea582101d
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2025-04-09 16:43:15 +0000

    added the softdelete manager

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.db.models.query import QuerySet
 # Create your models here.
+class SoftDeleteManager(models.Manager):
+    '''
+    Getting QuerySet function for soft delet models
+    '''
+
+    return super().get_queryset().filter(active=True)
