c037549d53b3605557e51e99e46e3e761c54c002
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2025-04-10 13:33:59 +0000

    populated the blogs model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from core.models import SoftDeleteModel
 # Create your models here.
+class Blogs(SoftDeleteModel):
+    '''
+    This model will be used to create blogs
+    '''
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
